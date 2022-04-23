b7fbf60009884e0a1d52940252143d14e384d48e
Leoncenno <86723281+Leoncenno@users.noreply.github.com>
2022-04-24 00:38:21 +0300

    Revert "Test"

--- run.py
+++
@@ -1,6 +0,0 @@
-from app.app import app
-from app.models import Models
-
-
-if __name__ == '__main__':
-    app.run()
--- test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from tests.test_validators import test_validators
-from tests.test_api import TestApp
-from tests.test_user import TestUser
-
-if __name__ == '__main__':
-    unittest.main()
