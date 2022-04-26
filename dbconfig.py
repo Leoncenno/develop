2de174d0aaa18d8923ff510a7783699474705b21
Leonard Odongo <leonarddng@gmail.com>
2022-04-26 20:41:45 +0300

    configuring database url for travis

--- dbconfig.py
+++ dbconfig.py
@@ -12,4 +12,4 @@ class TestConfig(Config):
 
     DEBUG = False
     TESTING = True
-    DATABASE_URL = 'postgresql://postgres:root@localhost/StackOverflow_test'
+    DATABASE_URL = 'postgresql://postgres:root@localhost/StackOverflow'
