d7ceb44c301453138b592507bc31e4b7992d9bf9
Leonard Odongo <leonarddng@gmail.com>
2022-04-28 12:41:31 +0300

    renaming database

--- dbconfig.py
+++ dbconfig.py
@@ -6,10 +6,10 @@ class DevelopmentConfig(Config):
 
     DEBUG = True
     TESTNG = False
-    DATABASE_URL = 'postgresql://postgres:root@localhost/StackOverflow'
+    DATABASE_URL = 'postgresql://postgres:root@localhost/stackoverflow'
 
 class TestConfig(Config):
 
     DEBUG = False
     TESTING = True
-    DATABASE_URL = 'postgres://postgres:@localhost/StackOverflow'
+    DATABASE_URL = 'postgres://postgres:@localhost/stackoverflow'
